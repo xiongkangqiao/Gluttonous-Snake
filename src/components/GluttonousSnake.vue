80316cbec7c0e690b294f1e26aab4f81d14a2387
xiongkangqiao <3030199850@qq.com>
2021-06-08 21:41:47 +0800

    add snake move

--- src/components/GluttonousSnake.vue
+++ src/components/GluttonousSnake.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="project" style="position: relative;width: 100%;height: 540px;background-color: #3493cb">
+  <div class="project" style="position: relative;width: 100%;height: 753px;background-color: #3493cb">
     <div class="mapDiv" style="position: absolute;top: 50%;left: 50%;transform: translate(-50%, -50%);width: 300px;height: 300px;background-color: #3ba1c4">
 
     </div>
@@ -14,6 +14,11 @@ export default {
       msg: 'Welcome to Your Vue.js App',
       snakeBody: [[3, 1, 'head'], [2, 1, 'body'], [1, 1, 'body']],
       direct:'right',
+      snakeMove:null,
+      left: false,
+      right: false,
+      up: true,
+      down: true,
     }
   },
   methods:{
@@ -44,10 +49,111 @@ export default {
             break;
         }
       }
-    }
+    },
+    move(){
+      console.log(this.snakeBody.toString())
+      for (var i = this.snakeBody.length - 1; i > 0; i--) {
+        this.snakeBody[i][0] = this.snakeBody[i - 1][0];
+        this.snakeBody[i][1] = this.snakeBody[i - 1][1];
+      }
+      // console.log(this.snakeBody.toString())
+      // 假设已经运动，存放方向
+      switch (this.direct) {
+        case 'right':
+          this.snakeBody[0][0] += 1;
+          break;
+        case 'up':
+          this.snakeBody[0][1] -= 1;
+          break;
+        case 'left':
+          this.snakeBody[0][0] -= 1;
+          break;
+        case 'down':
+          this.snakeBody[0][1] += 1;
+          break;
+        default:
+          break;
+      }
+      if (this.snakeBody[0][0] < 0 || this.snakeBody[0][0] >= 300 / 20) {
+        // console.log(1)
+        clearInterval(this.snakeMove)
+        return
+
+      }
+
+      if (this.snakeBody[0][1] < 0 || this.snakeBody[0][1] >= 300 / 20) {
+        // console.log(1)
+        clearInterval(this.snakeMove)
+        return
+
+      }
+      this.removeClass('snake');
+      this.init();
+
+
+    },
+    game(){
+      this.snakeMove = setInterval(()=> {
+        this.move()
+      }, 200)
+    },
+    directChange(){
+      document.onkeydown = (ev)=> {
+        var ev = ev || window.event;
+        var code = ev.keyCode;
+        switch (code) {
+          case 37:
+            if (this.left) {
+              this.direct = 'left';
+              this.left = false;
+              this.right = false;
+              this.up = true;
+              this.down = true;
+            }
+            break;
+          case 38:
+            if (this.up) {
+              this.direct = 'up';
+              this.left = true;
+              this.right = true;
+              this.up = false;
+              this.down = false;
+            }
+            break;
+          case 39:
+            if (this.right) {
+              this.direct = 'right';
+              this.left = false;
+              this.right = false;
+              this.up = true;
+              this.down = true;
+            }
+            break;
+          case 40:
+            if (this.down) {
+              this.direct = 'down';
+              this.left = true;
+              this.right = true;
+              this.up = false;
+              this.down = false;
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    },
+    removeClass(className) {
+      var ele = document.getElementsByClassName(className);
+      while (ele.length > 0) {
+        ele[0].parentNode.removeChild(ele[0]);
+      }
+    },
   },
   mounted() {
     this.init()
+    this.game()
+    this.directChange()
   }
 }
 </script>
